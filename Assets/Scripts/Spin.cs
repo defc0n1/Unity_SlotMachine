4adbd7bc6b0398ab333851db5662b0bab17faf60
David Barbera <davidbarbera9@gmail.com>
2016-01-27 16:31:53 +0000

    Basic Spin

---
+++ Assets/Scripts/Spin.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spin : MonoBehaviour {
+
+    private bool isSpinning = false;
+    private float spinCounter = 0;
+    private float spinStep = 360.0f / 4.0f;
+
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.Space) && !isSpinning)
+        {
+            isSpinning = !isSpinning;
+            spinCounter = Random.Range(90, 150);
+        }
+
+        if (isSpinning && spinCounter > 0)
+        {
+            transform.Rotate(new Vector3(0, 0, 1), spinStep);
+            --spinCounter;
+        } else if (isSpinning && spinCounter <= 0)
+        {
+            isSpinning = false;
+        }
+	}
+}
