cc028c2c59566e800499f346b89836c077f14d7b
roba90 <ralle015@gold.ac.uk>
2016-01-28 19:41:01 +0000

    Animation Script

---
+++ Assets/Scripts/PlayAnimation.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayAnimation : MonoBehaviour
+{
+
+    void update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+            Animation.Play();
+    }
+}
+	
