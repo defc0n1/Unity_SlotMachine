a5626688fedaa4b09df266853e070f3580c781fc
roba90 <ralle015@gold.ac.uk>
2016-01-29 17:22:19 +0000

    Handle

---
+++ Assets/Scripts/Handle.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Handle : MonoBehaviour
+{
+    void update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.Rotate(new Vector3(0, 0, 1), 60);
+        }
+    }
+}
+    
